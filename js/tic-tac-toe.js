eb4e179d1d695f3de9dbf963c6e4db7f85491488
vworo <victorworobec@gmail.com>
2023-04-03 15:31:22 -0700

    Added basic style and placeholder event listeners

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,2 +1,32 @@
 console.log('test', $.fn.jquery);
 
+$(document).ready( function () {
+
+    $('#top-left').on('click', function () {
+        $(this).html('x');
+    });
+    $('#top-middle').on('click', function () {
+        $(this).html('x');
+    });
+    $('#top-right').on('click', function () {
+        $(this).html('x');
+    });
+    $('#middle-left').on('click', function () {
+        $(this).html('x');
+    });
+    $('#middle-middle').on('click', function () {
+        $(this).html('x');
+    });
+    $('#middle-right').on('click', function () {
+        $(this).html('x');
+    });
+    $('#bottom-left').on('click', function () {
+        $(this).html('x');
+    });
+    $('#bottom-middle').on('click', function () {
+        $(this).html('x');
+    });
+    $('#bottom-right').on('click', function () {
+        $(this).html('x');
+    });
+});
