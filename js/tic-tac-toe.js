517709272a63337bcb11c1abf7404543aebc5d2c
vworo <victorworobec@gmail.com>
2023-04-03 22:21:15 -0700

    Added "placeInput" function: places innerhtml based on current turn

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,13 +1,32 @@
-console.log('test', $.fn.jquery);
+let currentTurn = "PLAYER1";
 
 const placeInput = function (event) {
     const squareDiv = event.target;
-    $(squareDiv).html('x');
+
+    if (currentTurn === "PLAYER1") {
+        if ($(squareDiv).html() != 'x' && $(squareDiv).html() != 'o') {
+            $(squareDiv).html('x');
+            currentTurn = "PLAYER2";
+        }
+        return;
+    }
+
+    else if (currentTurn === "PLAYER2") {
+        if ($(squareDiv).html() != 'x' && $(squareDiv).html() != 'o') {
+            $(squareDiv).html('o');
+            currentTurn = "PLAYER1";
+        }
+    }
+
+    else if (currentTurn === "AI") {
+        $(squareDiv).html('o');
+        currentTurn = "PLAYER1";
+    }
 }
 
 $(document).ready( function () {
 
     $('.square').on('click', function (event) {
         placeInput(event);
-    })
+    });
 });
